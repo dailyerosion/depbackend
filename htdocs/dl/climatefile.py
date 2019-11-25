97fb75ff0998047f508c616db9a51b9203ef1624
akrherz <akrherz@iastate.edu>
2019-11-25 10:03:31 -0600

    adjust the clifile search algo

--- htdocs/dl/climatefile.py
+++ htdocs/dl/climatefile.py
@@ -9,21 +9,19 @@ from pyiem.util import ssw
 
 def spiral(lon, lat):
     """https://stackoverflow.com/questions/398299/looping-in-a-spiral"""
-    N = 26
-    x, y = 0, 0
-    dx, dy = 0, -1
-
-    for _ in range(N * N):
-        if abs(x) == abs(y) and [dx, dy] != [1, 0] or x > 0 and y == 1 - x:
-            dx, dy = -dy, dx            # corner, change direction
-
-        if abs(x) > N / 2 or abs(y) > N / 2:  # non-square
-            dx, dy = -dy, dx                  # change direction
-            x, y = -y + dx, x + dy            # jump
-
-        newfn = get_cli_fname(lon + x * 0.01, lat + y * 0.01)
-        if os.path.isfile(newfn):
-            return newfn
+    x = y = 0
+    dx = 0
+    dy = -1
+    # points near the domain edge need to seach a bit further than 0.25deg
+    X = 40
+    Y = 40
+    for _ in range(40 ** 2):
+        if (-X / 2 < x <= X / 2) and (-Y / 2 < y <= Y / 2):
+            newfn = get_cli_fname(lon + x * 0.01, lat + y * 0.01)
+            if os.path.isfile(newfn):
+                return newfn
+        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1 - y):
+            dx, dy = -dy, dx
         x, y = x + dx, y + dy
     return None
 
