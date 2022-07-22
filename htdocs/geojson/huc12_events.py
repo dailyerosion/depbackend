6cb1c30eb0fa0b60e29c0ddcec5d259ca675b670
akrherz <akrherz@iastate.edu>
2022-07-22 08:39:25 -0500

    iterate as depmap needs

--- htdocs/geojson/huc12_events.py
+++ htdocs/geojson/huc12_events.py
@@ -1,64 +1,91 @@
 """GeoJSON service for HUC12 data"""
-import json
 import datetime
+from io import BytesIO
+import json
 
+import pandas as pd
 from pymemcache.client import Client
 from paste.request import parse_formvars
-from pyiem.util import get_dbconn
+from pyiem.util import get_sqlalchemy_conn
+
+EXL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
 
-def do(huc12, mode):
+def do(huc12, mode, fmt):
     """Do work"""
-    pgconn = get_dbconn("idep")
-    cursor = pgconn.cursor()
     utcnow = datetime.datetime.utcnow()
     if mode == "daily":
-        cursor.execute(
-            """
-            SELECT valid, avg_loss * 4.463, avg_delivery * 4.463,
-            qc_precip / 25.4, avg_runoff / 25.4, 1, 1, 1, 1
-            from results_by_huc12 where huc_12 = %s and scenario = 0 ORDER
-            by valid ASC
-        """,
-            (huc12,),
-        )
+        with get_sqlalchemy_conn("idep") as conn:
+            df = pd.read_sql(
+                """
+                SELECT valid,
+                avg_loss * 4.463 as avg_loss,
+                avg_delivery * 4.463 as avg_delivery,
+                qc_precip / 25.4 as qc_precip,
+                avg_runoff / 25.4 as avg_runoff,
+                1 as avg_loss_events,
+                1 as avg_delivery_events,
+                1 as qc_precip_events,
+                1 as avg_runoff_events
+                from results_by_huc12 where huc_12 = %s and scenario = 0 ORDER
+                by valid ASC
+            """,
+                conn,
+                params=(huc12,),
+                index_col=None,
+            )
     else:
-        cursor.execute(
-            """
-            SELECT extract(year from valid)::int as yr,
-            sum(avg_loss) * 4.463,
-            sum(avg_delivery) * 4.463,
-            sum(qc_precip) / 25.4,
-            sum(avg_runoff) / 25.4,
-            sum(case when avg_loss > 0 then 1 else 0 end),
-            sum(case when avg_delivery > 0 then 1 else 0 end),
-            sum(case when qc_precip > 0 then 1 else 0 end),
-            sum(case when avg_runoff > 0 then 1 else 0 end)
-            from results_by_huc12 where huc_12 = %s and scenario = 0
-            GROUP by yr ORDER by yr ASC
-        """,
-            (huc12,),
-        )
+        with get_sqlalchemy_conn("idep") as conn:
+            df = pd.read_sql(
+                """
+                SELECT extract(year from valid)::int as yr,
+                sum(avg_loss) * 4.463 as avg_loss,
+                sum(avg_delivery) * 4.463 as avg_delivery,
+                sum(qc_precip) / 25.4 as qc_precip,
+                sum(avg_runoff) / 25.4 as avg_runoff,
+                sum(case when avg_loss > 0 then 1 else 0 end)
+                    as avg_loss_events,
+                sum(case when avg_delivery > 0 then 1 else 0 end)
+                    as avg_delivery_events,
+                sum(case when qc_precip > 0 then 1 else 0 end)
+                    as qc_precip_events,
+                sum(case when avg_runoff > 0 then 1 else 0 end)
+                    as avg_runoff_events
+                from results_by_huc12 where huc_12 = %s and scenario = 0
+                GROUP by yr ORDER by yr ASC
+            """,
+                conn,
+                params=(huc12,),
+                index_col=None,
+            )
+    if fmt == "xlsx":
+        bio = BytesIO()
+        # pylint: disable=abstract-class-instantiated
+        writer = pd.ExcelWriter(bio, engine="xlsxwriter")
+        df.to_excel(writer, f"{huc12} Data", index=False)
+        writer.close()
+        return bio.getvalue()
+
     res = {
         "results": [],
         "huc12": huc12,
         "generation_time": utcnow.strftime("%Y-%m-%dT%H:%M:%SZ"),
     }
-    for row in cursor:
+    for _, row in df.iterrows():
         dt = row[0]
         if isinstance(row[0], int):
             dt = datetime.date(int(row[0]), 1, 1)
         res["results"].append(
             dict(
-                date=dt.strftime("%m/%d/%Y"),
-                avg_loss=row[1],
-                avg_loss_events=row[5],
-                avg_delivery=row[2],
-                avg_delivery_events=row[6],
-                qc_precip=row[3],
-                qc_precip_events=row[7],
-                avg_runoff=row[4],
-                avg_runoff_events=row[8],
+                date=dt.strftime("%Y-%m-%d"),
+                avg_loss=row["avg_loss"],
+                avg_loss_events=row["avg_loss_events"],
+                avg_delivery=row["avg_delivery"],
+                avg_delivery_events=row["avg_delivery_events"],
+                qc_precip=row["qc_precip"],
+                qc_precip_events=row["qc_precip_events"],
+                avg_runoff=row["avg_runoff"],
+                avg_runoff_events=row["avg_runoff_events"],
             )
         )
     return json.dumps(res)
@@ -66,18 +93,29 @@ def do(huc12, mode):
 
 def application(environ, start_response):
     """Do Fun things"""
-    headers = [("Content-Type", "application/vnd.geo+json")]
-    start_response("200 OK", headers)
     form = parse_formvars(environ)
     cb = form.get("callback", None)
     huc12 = form.get("huc12", "000000000000")[:12]
     mode = form.get("mode", "daily")
+    fmt = form.get("format", "json")
 
-    mckey = f"/geojson/huc12_events/{huc12}/{mode}"
+    if fmt == "json":
+        headers = [("Content-Type", "application/vnd.geo+json")]
+    elif fmt == "xlsx":
+        headers = [
+            ("Content-Type", EXL),
+            ("Content-disposition", f"attachment; Filename=dep{huc12}.xlsx"),
+        ]
+    start_response("200 OK", headers)
+
+    mckey = f"/geojson/huc12_events/{huc12}/{mode}/{fmt}"
     mc = Client(["iem-memcached", 11211])
     res = mc.get(mckey)
     if res is None:
-        res = do(huc12, mode)
+        res = do(huc12, mode, fmt)
+        if fmt == "xlsx":
+            mc.close()
+            return [res]
         mc.set(mckey, res, 15)
     else:
         res = res.decode("utf-8")
