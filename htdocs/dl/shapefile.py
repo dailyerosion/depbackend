86d3b43bde5fd018b707858b1c5e4f915403d498
akrherz <akrherz@iastate.edu>
2023-02-20 07:57:02 -0600

    shapefile backend improvements
    - modernize
    - add csv to export
    - add tillcode and avg_slp1 #18

--- htdocs/dl/shapefile.py
+++ htdocs/dl/shapefile.py
@@ -3,80 +3,86 @@ Called from DEP map application
 """
 import datetime
 import os
-import shutil
+import tempfile
 import zipfile
 
 from geopandas import GeoDataFrame
 from paste.request import parse_formvars
-from pyiem.util import get_dbconn
+from sqlalchemy import text
+from pyiem.util import get_sqlalchemy_conn
+
+PRJFILE = "/opt/iem/data/gis/meta/5070.prj"
 
 
 def workflow(start_response, dt, dt2, states):
     """Generate for a given date"""
-    dbconn = get_dbconn("idep")
-    dextra = "valid = %s"
-    args = (dt,)
+    dextra = "valid = :dt"
+    params = {"dt": dt}
     if dt2 is not None:
-        dextra = "valid >= %s and valid <= %s"
-        args = (dt, dt2)
+        dextra = "valid >= :dt and valid <= :dt2"
+        params["dt2"] = dt2
     statelimit = ""
     if states is not None:
         tokens = states.split(",")
         if tokens:
-            _s = [" states ~* '%s' " % (a,) for a in tokens]
+            _s = [f" states ~* '{a}' " for a in tokens]
             statelimit = " and (" + " or ".join(_s) + " ) "
-    df = GeoDataFrame.from_postgis(
-        f"""
-        WITH data as (
-            SELECT simple_geom, huc_12, name
-            from huc12 WHERE scenario = 0
-            {statelimit}),
-        obs as (
-            SELECT huc_12,
-            sum(coalesce(avg_loss, 0)) as avg_loss,
-            sum(coalesce(avg_delivery, 0)) as avg_delivery,
-            sum(coalesce(qc_precip, 0)) as qc_precip,
-            sum(coalesce(avg_runoff, 0)) as avg_runoff
-            from results_by_huc12 WHERE
-            {dextra} and scenario = 0 GROUP by huc_12)
+    with get_sqlalchemy_conn("idep") as conn:
+        df = GeoDataFrame.from_postgis(
+            text(
+                f"""
+            WITH data as (
+                SELECT simple_geom, huc_12, name, dominant_tillage,
+                average_slope_ratio from huc12 WHERE scenario = 0
+                {statelimit}),
+            obs as (
+                SELECT huc_12,
+                sum(coalesce(avg_loss, 0)) as avg_loss,
+                sum(coalesce(avg_delivery, 0)) as avg_delivery,
+                sum(coalesce(qc_precip, 0)) as qc_precip,
+                sum(coalesce(avg_runoff, 0)) as avg_runoff
+                from results_by_huc12 WHERE
+                {dextra} and scenario = 0 GROUP by huc_12)
 
-        SELECT d.simple_geom, d.huc_12, d.name,
-        coalesce(o.qc_precip, 0) as prec_mm,
-        coalesce(o.avg_loss, 0) as los_kgm2,
-        coalesce(o.avg_runoff, 0) as runof_mm,
-        coalesce(o.avg_delivery, 0) as deli_kgm
-        from data d LEFT JOIN obs o ON (d.huc_12 = o.huc_12)
-    """,
-        dbconn,
-        params=args,
-        geom_col="simple_geom",
-    )
+            SELECT d.simple_geom as geo, d.huc_12, d.name,
+            d.dominant_tillage as tillcode,
+            d.average_slope_ratio as avg_slp1,
+            coalesce(o.qc_precip, 0) as prec_mm,
+            coalesce(o.avg_loss, 0) as los_kgm2,
+            coalesce(o.avg_runoff, 0) as runof_mm,
+            coalesce(o.avg_delivery, 0) as deli_kgm
+            from data d LEFT JOIN obs o ON (d.huc_12 = o.huc_12)
+        """
+            ),
+            conn,
+            params=params,
+            geom_col="geo",
+        )
 
-    os.chdir("/tmp")
-    fn = "idepv2_%s" % (dt.strftime("%Y%m%d"),)
-    if dt2:
-        fn += dt2.strftime("_%Y%m%d")
-    df.columns = [
-        s.upper() if s != "simple_geom" else s for s in df.columns.values
-    ]
-    df.to_file(fn + ".shp")
-    shutil.copyfile("/opt/iem/data/gis/meta/5070.prj", fn + ".prj")
-    zfp = zipfile.ZipFile(fn + ".zip", "w", zipfile.ZIP_DEFLATED)
-    suffixes = ["shp", "shx", "dbf", "prj"]
-    for suffix in suffixes:
-        zfp.write("%s.%s" % (fn, suffix))
-    zfp.close()
+    with tempfile.TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)
+        fn = f"idepv2_{dt:%Y%m%d}"
+        if dt2:
+            fn += dt2.strftime("_%Y%m%d")
+        df.columns = [
+            s.upper() if s != "geo" else s for s in df.columns.values
+        ]
+        df.to_file(f"{fn}.shp")
+        df.drop(columns="geo").to_csv(f"{fn}.csv", index=False)
+        with zipfile.ZipFile(fn + ".zip", "w", zipfile.ZIP_DEFLATED) as zfp:
+            with open(PRJFILE, encoding="ascii") as fh:
+                zfp.writestr(f"{fn}.prj", fh.read())
+            for suffix in ["shp", "shx", "dbf", "csv"]:
+                zfp.write(f"{fn}.{suffix}")
 
+        with open(f"{fn}.zip", "rb") as fh:
+            res = fh.read()
     headers = [
         ("Content-type", "application/octet-stream"),
-        ("Content-Disposition", "attachment; filename=%s.zip" % (fn,)),
+        ("Content-Disposition", f"attachment; filename={fn}.zip"),
     ]
     start_response("200 OK", headers)
-    res = open(fn + ".zip", "rb").read()
 
-    suffixes.append("zip")
-    for suffix in suffixes:
-        os.remove("%s.%s" % (fn, suffix))
     return res
 
 
