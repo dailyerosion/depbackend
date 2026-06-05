1aa09211fc6c7e6d0ac7e164030f4bb9fd96b85f
akrherz <akrherz@iastate.edu>
2026-06-05 08:40:00 -0500

    🎨 Improve pyproject to make install mostly work

--- pylib/depbackend/__init__.py
+++ pylib/depbackend/__init__.py
@@ -1,1 +1,13 @@
 """Importable."""
+
+import os
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("depbackend")
+    pkgdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    if not pkgdir.endswith("site-packages"):
+        __version__ += "-dev"
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "dev"
